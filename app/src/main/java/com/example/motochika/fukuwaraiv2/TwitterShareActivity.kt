244623f5a8c9922af9ccffbff54d16f7010f1c3c
広瀬元周 <3motochika@gmail.com>
2020-10-24 17:17:54 +0900

    実験的にTwitter投稿、画像の投稿に苦労する

---
+++ app/src/main/java/com/example/motochika/fukuwaraiv2/TwitterShareActivity.kt
@@ -0,0 +1,96 @@
+package com.example.motochika.fukuwaraiv2
+
+import android.content.ContentResolver
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.nfc.Tag
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Message
+import android.util.Log
+import android.widget.Toast
+import androidx.core.net.toUri
+import kotlinx.android.synthetic.main.activity_twitter_share.*
+import kotlinx.android.synthetic.main.fragment_entry.*
+import java.io.IOException
+import java.io.UnsupportedEncodingException
+import java.net.URLEncoder
+
+class TwitterShareActivity : AppCompatActivity() {
+
+    private lateinit var imageUri: Uri
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_twitter_share)
+
+        try {
+
+            val bitmap = BitmapFactory.decodeStream(assets.open("myface.jpg"))
+            imageUri = Uri.parse(bitmap.toString())
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            share_button.setOnClickListener {
+                shareTwitter("Test", imageUri)
+            }
+        }
+
+
+
+
+    }
+
+
+    private fun shareTwitter(message: String, u: Uri) {
+        val twitterIntent = Intent(Intent.ACTION_SEND).apply {
+            putExtra(Intent.EXTRA_STREAM, u)
+            type = "image/jpeg"
+        }
+
+        val packageManager = packageManager
+        val resolvedInfoList: List<ResolveInfo> = packageManager.queryIntentActivities(twitterIntent, PackageManager.MATCH_DEFAULT_ONLY)
+        var isResolved = false
+
+        for (resolveInfo in resolvedInfoList) {
+            if (resolveInfo.activityInfo.packageName.startsWith("com.twitter.android")) {
+                twitterIntent.setClassName(
+                    resolveInfo.activityInfo.packageName,
+                    resolveInfo.activityInfo.name
+                )
+                isResolved = true
+                break
+            }
+        }
+
+        if (isResolved) {
+            startActivity(Intent.createChooser(twitterIntent, "test"))
+        } else {
+           Intent().apply {
+               putExtra(Intent.EXTRA_TEXT, message)
+               action = Intent.ACTION_VIEW
+               data = Uri.parse("https://twitter.com/intent/tweet?text=" + urlEncode(message))
+           }
+
+            startActivity(Intent.createChooser(twitterIntent, "test"))
+            Toast.makeText(this, "TwitterApp is not found.", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    private fun urlEncode(url: String): String {
+
+        return try {
+            URLEncoder.encode(url, "UTF-8")
+
+        } catch (e: UnsupportedEncodingException) {
+            Log.w("TwitterShareActivity", "UTF-8 should always be supported", e)
+
+            ""
+        }
+    }
+}
--- app/src/main/java/com/example/motochika/fukuwaraiv2/entry/EntryFragment.kt
+++ app/src/main/java/com/example/motochika/fukuwaraiv2/entry/EntryFragment.kt
@@ -1,5 +1,6 @@
 package com.example.motochika.fukuwaraiv2.entry
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.motochika.fukuwaraiv2.R
+import com.example.motochika.fukuwaraiv2.TwitterShareActivity
 import kotlinx.android.synthetic.main.fragment_entry.view.*
 
 class EntryFragment : Fragment() {
@@ -28,6 +30,15 @@ class EntryFragment : Fragment() {
         view.entry_hyottoko_button.setOnClickListener {
 
             findNavController().navigate(R.id.action_entryFragment_to_secondFaceFragment)
+        }
+
+        view.test_button.setOnClickListener {
+
+            val intent = Intent(activity, TwitterShareActivity::class.java)
+            startActivity(intent)
+
+
+
         }
 
         return view
