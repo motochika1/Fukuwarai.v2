4c0fd29ff8f117c696f1f41d375f2b8acfebd773
MOTCCi <3motochika@gmail.com>
2020-12-29 18:28:30 +0900

    不要なコメント箇所削除

--- app/src/main/java/com/release/motochika/fukuwaraiv2/hyottoko/HyottokoFaceFragment.kt
+++ app/src/main/java/com/release/motochika/fukuwaraiv2/hyottoko/HyottokoFaceFragment.kt
@@ -1,23 +1,11 @@
 package com.release.motochika.fukuwaraiv2.hyottoko
 
-import android.app.Activity
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.content.pm.ResolveInfo
-import android.graphics.Bitmap
-import android.graphics.Color
-import android.net.Uri
 import android.os.*
 import android.util.Log
 import android.view.*
 import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import com.release.motochika.fukuwaraiv2.*
 import kotlinx.android.synthetic.main.fragment_hyottoko_face.*
 import kotlinx.android.synthetic.main.fragment_hyottoko_face.back_button
@@ -26,10 +14,6 @@ import kotlinx.android.synthetic.main.fragment_hyottoko_face.leftEye_image
 import kotlinx.android.synthetic.main.fragment_hyottoko_face.mouth_image
 import kotlinx.android.synthetic.main.fragment_hyottoko_face.nose_image
 import kotlinx.android.synthetic.main.fragment_hyottoko_face.rightEye_image
-import java.io.File
-import java.io.FileOutputStream
-import java.io.UnsupportedEncodingException
-import java.net.URLEncoder
 
 @RequiresApi(Build.VERSION_CODES.O)
 class HyottokoFaceFragment : Fragment() {
@@ -57,15 +41,6 @@ class HyottokoFaceFragment : Fragment() {
                 "nose" to nose_image, "mouth" to mouth_image)
             val rootParts = mapOf<String, View>("face" to hyottoko_face, "root" to hyottoko_root, "eye-brows" to eyebrows)
             val buttons = mapOf<String, Button>("play" to changeFace_button, "play-again" to back_button)
-
-//            val rightEyeX = faceParts["right-eye"]?.x
-//            val rightEyeY = faceParts["right-eye"]?.y
-//            val leftEyeX = faceParts["left-eye"]?.x
-//            val leftEyeY = faceParts["left-eye"]?.y
-//            val noseX = faceParts["nose"]?.x
-//            val noseY = faceParts["nose"]?.y
-//            val mouthX = faceParts["mouth"]?.x
-//            val mouthY = faceParts["mouth"]?.y
 
 
             changeFace_button?.setOnClickListener {
@@ -100,11 +75,6 @@ class HyottokoFaceFragment : Fragment() {
                 listener.i = 0
 
                 faceParts.map { it.value.rotation = 0.toFloat() }
-
-//                (faceParts["right-eye"] ?: error("")).rotation = 0.toFloat()
-//                (faceParts["left-eye"] ?: error("")).rotation = 0.toFloat()
-//                (faceParts["nose"] ?: error("")).rotation = 0.toFloat()
-//                (faceParts["mouth"] ?: error("")).rotation = 0.toFloat()
 
                 changeFace_button.text = "あそぶ"
                 back_button.visibility = View.INVISIBLE
