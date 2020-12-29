80a107a2e58fca18dcb355c217dd6a7b82a35e71
MOTCCi <3motochika@gmail.com>
2020-12-29 18:53:35 +0900

    リスナのコードリフォーマット

--- app/src/main/java/com/release/motochika/fukuwaraiv2/ClickHandler.kt
+++ app/src/main/java/com/release/motochika/fukuwaraiv2/ClickHandler.kt
@@ -17,7 +17,11 @@ class ClickHandler {
     private lateinit var imageView: ImageView
 
     @RequiresApi(Build.VERSION_CODES.O)
-    fun playClicked(listener: Listener, faceParts: Map<String, View>, buttons: Map<String, Button>) {
+    fun playClicked(
+        listener: Listener,
+        faceParts: Map<String, View>,
+        buttons: Map<String, Button>
+    ) {
 
         //画像を透明にしている
 
@@ -25,19 +29,7 @@ class ClickHandler {
             it.value.alpha = 0.0.toFloat()
             it.value.setOnTouchListener(listener.getListener())
         }
-
-//        rightEye_image.alpha = 0.0.toFloat()
-//        leftEye_image.alpha = 0.0.toFloat()
-//        nose_image.alpha = 0.0.toFloat()
-//        mouth_image.alpha = 0.0.toFloat()
-
-//        rightEye_image.setOnTouchListener(listener.getListener())
-//        leftEye_image.setOnTouchListener(listener.getListener())
-//        nose_image.setOnTouchListener(listener.getListener())
-//        mouth_image.setOnTouchListener(listener.getListener())
-
         (buttons["play"] ?: error("")).text = "公開"
-
     }
 
     fun showFaceClicked(faceParts: Map<String, View>, buttons: Map<String, Button>) {
@@ -48,24 +40,17 @@ class ClickHandler {
             it.value.setOnTouchListener(null)
         }
 
-//        //元の透明度に戻している
-//        rightEye_image.alpha = 1.0.toFloat()
-//        leftEye_image.alpha = 1.0.toFloat()
-//        nose_image.alpha = 1.0.toFloat()
-//        mouth_image.alpha = 1.0.toFloat()
-
-//        rightEye_image.setOnTouchListener(null)
-//        leftEye_image.setOnTouchListener(null)
-//        nose_image.setOnTouchListener(null)
-//        mouth_image.setOnTouchListener(null)
-
         (buttons["play"] ?: error("")).text = "シェア"
         (buttons["play-again"] ?: error("")).visibility = View.VISIBLE
-
     }
 
-    fun shareClicked(screenShots: ScreenShots, twitterShare: TwitterShare,
-                             faceParts: Map<String, View>, rootParts: Map<String, View>, activity: Activity, buttons: Map<String, Button>
+    fun shareClicked(
+        screenShots: ScreenShots,
+        twitterShare: TwitterShare,
+        faceParts: Map<String, View>,
+        rootParts: Map<String, View>,
+        activity: Activity,
+        buttons: Map<String, Button>
     ) {
         root = rootParts["root"] ?: error("")
         imageView = rootParts["face"] as ImageView
@@ -79,17 +64,6 @@ class ClickHandler {
         (rootParts["eye-brows"] ?: error("")).visibility = View.INVISIBLE
         faceParts.map { it.value.visibility = View.INVISIBLE }
 
-//        eyebrows.visibility = View.INVISIBLE
-//        rightEye_image.visibility = View.INVISIBLE
-//        leftEye_image.visibility = View.INVISIBLE
-//        mouth_image.visibility = View.INVISIBLE
-//        nose_image.visibility = View.INVISIBLE
-
-
-        //screenShots.saveScreenShot(requireActivity(), bitmap)
-        twitterShare.shareTwitter("Share",bitmap, activity)
+        twitterShare.shareTwitter("Share", bitmap, activity)
     }
-
-
-
 }
